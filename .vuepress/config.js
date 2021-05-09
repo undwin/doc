0ee5a3114adf3f0a1f5be7a1cf2e144d9bc23498
undwin <love@und.win>
2021-05-10 01:14:43 +0800

    config.js

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+        ['vuepress-plugin-yuque', {
+            repoUrl: 'https://www.yuque.com/ukz96y/ngd5zk',
+            authToken:'NdzST5UU7oIbd3ugj8LtqHip4KxosEEYHBmizIC4',
+            home: {
+                actionText: '入门 →',
+                actionLink: '/intro/',
+                heroImage: 'http://www.wzlue.com/include/wzl/images/footer_logo.png',
+                footer: `Copyright © 微战略`
+            },
+            html:true
+        }]
+    ]
+}
